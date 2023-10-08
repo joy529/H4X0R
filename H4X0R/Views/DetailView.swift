01d2118808c5cbd03a7ad44e6e586f8466cf5fa6
joy jain <joyshah204@gmial.com>
2023-10-08 23:23:49 +0530

    url google is working after clicking.

--- H4X0R/Views/DetailView.swift
+++ H4X0R/Views/DetailView.swift
@@ -6,16 +6,42 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct DetailView: View {
     
     let url: String?
     
     var body: some View {
-        Text("Hello, World!")
+        WebView(urlString: url)
     }
 }
 
 #Preview {
     DetailView(url: "https://www.google.com")
 }
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WebView.UIViewType {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
