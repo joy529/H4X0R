0c2078610b9a40928a34cd943744d87a65ab3ba2
joy jain <joyshah204@gmial.com>
2023-10-08 22:59:33 +0530

    fetch the data from api is working

--- H4X0R/ContentView.swift
+++ H4X0R/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
     var body: some View {
         NavigationView {
-            List (posts) { post in
-                Text(post.title)
+            List (networkManager.posts) { post in
+                HStack {
+                    Text(String(post.points))
+                    Text(post.title)
+                }
             }
             .navigationBarTitle("H4X0R")
+        }
+        .onAppear {
+            networkManager.fetchData()
         }
     }
 }
@@ -22,16 +31,12 @@ struct ContentView: View {
     ContentView()
 }
 
-struct Post: Identifiable {
-    let id: String
-    let title: String
-}
 
-let posts = [
-    Post(id: "1", title: "Hello"),
-    Post(id: "2", title: "Bonjour"),
-    Post(id: "3", title: "Namaste")
-]
+//let posts = [
+//    Post(id: "1", title: "Hello"),
+//    Post(id: "2", title: "Bonjour"),
+//    Post(id: "3", title: "Namaste")
+//]
 
 
 
---
+++ H4X0R/Models/PostData.swift
@@ -0,0 +1,27 @@
+//
+//  PostData.swift
+//  H4X0R
+//
+//  Created by JOY JAIN on 08/10/23.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String?
+}
+
+
+
+
+
